720097440ef452047b312e0d48137d1a272ba671
xzzt <xuzhou65728220>
2017-11-21 17:17:26 +0800

    1121

--- xzazt-web/src/main/java/com/xzazt/controller/UserController.java
+++ xzazt-web/src/main/java/com/xzazt/controller/UserController.java
@@ -5,11 +5,11 @@ import com.xzazt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
@@ -20,9 +20,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(value = "/getUser",method = RequestMethod.POST)
+    @RequestMapping(value = "/getUser")
     @ResponseBody
-    public String getUser(){
+    public String getUser(HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods","POST");
+        response.setHeader("Access-Control-Allow-Headers","x-requested-with,content-type");
         List<Map<String, Object>> user = userService.getUser();
         String s = JSON.toJSONString(user);
         return s;
